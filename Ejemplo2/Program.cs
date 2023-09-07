c3e08129b5db1e2a3e6de72dde4f95e1286f870e
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-09-06 20:51:04 -0600

    Consultas

---
+++ Ejemplo2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+class Consulta
+{
+    static void Main()
+    {
+        int[] score = { 90, 71, 82, 93, 75, 82 };
+
+        var Query =
+            from num in score
+            where num > 80
+            orderby num descending
+            select num;
+
+        Console.WriteLine("Resultados de la consulta: ");
+        foreach (var item in Query)
+        {
+            Console.WriteLine($" - {item}");
+        }
+
+    }
+
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+class Consulta
+{
+    static void Main()
+    {
+        int[] scores = { 97, 92, 81, 60 };
+
+        var Query =
+            from num in scores
+            where num > 80
+            select num;
+
+        Console.WriteLine("Resultado de la consulta:");
+        foreach (var iten in Query)
+        {
+            Console.Write(" " + iten.ToString() );
+        }
+        Console.WriteLine("\n");
+    }
+}
